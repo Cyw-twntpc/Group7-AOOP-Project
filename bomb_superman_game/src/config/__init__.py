4373c9221e3551a5964ec1f43475c00f03a7876b
Cyw-twntpc <125878795+Cyw-twntpc@users.noreply.github.com>
2023-10-31 11:21:24 +0800

    add system config

--- bomb_superman_game/src/config/__init__.py
+++ bomb_superman_game/src/config/__init__.py
@@ -1,3 +1,12 @@
-#
-# @file __init__.py
-#
+import json
+
+pygame_config = {
+    'window_width': 800,
+    'window_height': 600,
+    'background_color': (0, 0, 0), 
+    'fps': 60,
+    'sound_volume': 0.8,
+}
+
+with open('./bomb_superman_game/src/config/config.json', 'w') as file:
+    json.dump(pygame_config, file)
